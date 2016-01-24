f88a450c267a028445a123504f80db6097130381
Stephen Saunders <saunders.stephen@gmail.com>
2016-01-23 16:59:15 -0800

    removing extra modules in Db.js

--- Server/Database/Db.js
+++ Server/Database/Db.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
-// var Mongo = require('../ApiKeys/mongolab.js');
+var Mongo = require('../ApiKeys/mongolab.js');
 var env = process.env.NODE_ENV || 'development';
-var User = require('../Models/user.js');
-
-var Trip = require('../Models/trip');
-var Video = require('../Models/video');
-var LocPointModel = require('./loc-point');
 
 if(env === 'development'){
 	mongoose.connect('mongodb://localhost:27017/lantern');
