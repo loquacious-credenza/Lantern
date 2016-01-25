bc97c2cb54edd2e75b399ad8cc499ef01ccdc4c0
Stephen Saunders <saunders.stephen@gmail.com>
2016-01-25 11:48:24 -0800

    Implement  method

---
+++ Server/Controllers/trip-controllers.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Trip = require('../Models/trip');
+
+/**
+ * Create a new trip with information from the response object.
+ * @param  {object} req  Request object decorated with params and body properties
+ * @param  {object} res  The response to client, either new record or error
+ * @return {undefined}      The server responds from within the promise.
+ */
+function create(req, res, data){
+  // Grab userId from the req params.
+  var user = req.params.user_id;
+
+  // Use the Trip models create method to create a new instance of trip
+  // and write to Mongo.
+  // Note that `path` and `videos` are empty at create time.
+  return Trip.create({
+    user_id: user,
+    destination: req.body.destination,
+    start_time: req.body.startTime,
+    overdue_time: req.body.overdueTime
+  }).then(function(newTrip){ //Once the new trip is created
+      console.log(newTrip); // console.log for debugging  REMOVE
+      res.status(201).json(newTrip); //send the object back to client with status 201
+    })
+    .catch(function(err){ // if there is an error
+      // TODO: be sure that server logs errors with logger
+      res.status(500).send('The trip could not be created', err); //send appropriate response to client.
+    });
+}
+
+module.exports = {
+  create: create
+};
