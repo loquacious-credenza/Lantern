9a3e3363e2db481c6044f72fc0b7acca81ce3196
Max O'Connell <moconnell7171@gmail.com>
2016-01-25 10:54:31 -0800

    Basic user methods created. Yet to be tested..

--- Server/Controllers/user-controller.js
+++ Server/Controllers/user-controller.js
@@ -1,8 +1,9 @@
 var User = require('../Models/user.js');
 
 module.exports = {
+	//FINDS 'user' BY ID AND RETURNS JSON
 	findUser: function (id, res) {
-		User.getOne(id, function (err, response) {
+		User.findById(id, function (err, response) {
 			if (err) {
 				console.log("Error finding user: ", err);
 				res.sendStatus(500);
@@ -10,5 +11,43 @@ module.exports = {
 				res.json(response);
 			}
 		});
-	};
+	},
+	// SEARCHES FOR 'user' BY ID INCLUDED IN 'userObj'. IF 'user' IS FOUND, IT IS RETURNED.
+	// IF NOT, 'user' IS CREATED IN THE DATABASE USING 'userObj' AND THEN RETURNED
+	findOrCreateUser: function (userObj, res) {
+		User.findById(userObj.user_id, function (err, response) {
+			if (err) {
+				console.log("Error finding user: ", err);
+				res.sendStatus(500);
+			} else if (response !== null) {
+				res.json(response);
+			} else {
+				User.create(userObj, function (err, response) {
+					if (err) {
+						console.log("Error creating user: ", err);
+						res.sendStatus(500);
+					} else {
+						res.json(response);
+					}
+				});
+			}
+		});
+	},
+	// SEARCHES FOR USER BY 'id' AND UPDATES CONTACTS USING 'contactsArr'. 'contactsArr' MUST
+	// BE AN ARRAY OF VALUES MATCHING THE SCHEMA OUTLINED IN 'contact.js'.
+	updateContacts: function (id, contactsArr, res) {
+		User.findByIdAndUpdate(id, {$set: {contacts: contactsArr}}, function (err, response) {
+			if (err) {
+				console.log("Error updating user contacts: ", err);
+				res.sendStatus(500);
+			} else {
+				res.json(response);
+			}
+		});
+	}
+
 }
+
+
+
+
