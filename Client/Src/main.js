54e3921c6ab10090f04412c75a678c9cd38a633f
Dakota-Peel <koteman624@gmail.com>
2016-01-26 19:48:32 -0800

    retrived users name from facebook and sent to home view

--- Client/Src/main.js
+++ Client/Src/main.js
@@ -1,11 +1,18 @@
 var React = require('react-native');
 var {
   StyleSheet,
-  Navigator
+  Navigator,
+  AsyncStorage
 } = React;
+var FBSDKCore = require('react-native-fbsdkcore');
+var {
+  FBSDKAccessToken,
+} = FBSDKCore;
 
 var Signin = require('./Facebook/Signin');
 var Home = require('./Home/Home');
+
+
 
 var ROUTES = {
   signin: Signin,
@@ -16,6 +23,12 @@ var ROUTES = {
  * A sample app that demonstrates use of the FBSDK login button, native share dialog, and graph requests.
  */
 var Main = React.createClass({
+  componentWillMount: function(){
+    var state = {route: 'signin', ID: null};
+
+    this.setState(state);
+
+  },
   renderScene: function(route, navigator){
     var Component = ROUTES[route.name];
     return <Component route={route} navigator={navigator}/>;
@@ -24,7 +37,7 @@ var Main = React.createClass({
     return (
       <Navigator
         style={styles.navigator}
-        initialRoute={{name: 'signin'}}
+        initialRoute={{name: this.state.route}}
         renderScene={this.renderScene}
         configureScene={() => {
           return Navigator.SceneConfigs.FloatFromRight; }}
