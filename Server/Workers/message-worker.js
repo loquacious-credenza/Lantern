ed72ee05786a8cbaa676273a16874fcf4d21e32c
Max O'Connell <moconnell7171@gmail.com>
2016-01-26 17:12:17 -0800

    Trips status is now set to 'false' once messages are sent out. Made small corrections to outgoing message and cleaned up comments.

--- Server/Workers/message-worker.js
+++ Server/Workers/message-worker.js
@@ -5,7 +5,7 @@ var mail = require('./email-methods.js');
 var cronJob = require('cron').CronJob;
 var async = require('async');
 
-// CALLS 'checkTrips' EVERY 20 SECONDS.
+// CALLS 'checkTrips' EVERY 30 SECONDS.
 var job = new cronJob({
 	cronTime: '*/30 * * * * *',
 	onTick: function () {
@@ -16,7 +16,8 @@ var job = new cronJob({
 });
 
 // CHECKS DATABASE PERIODICALLY FOR TRIPS THAT ARE BOTH EXPIRED AND ACTIVE. IF IT FINDS ONE, IT LOOKS UP
-// THE USER ASSOCIATED WITH THE TRIP, AND THEN PASSES BOTH TO 'pingTwilio'.
+// THE USER ASSOCIATED WITH THE TRIP, AND THEN PASSES BOTH TO 'twilio' & 'mail'. IT THEN SETS THE TRIP'S 
+// 'active' FIELD TO 'false'.
 var checkTrips = function () {
 	var currentTime = Date.now();
 	Trip.find({active:true, overdue_time: {$lt: currentTime}}, '-video -active', function (err, trips) {
@@ -30,6 +31,12 @@ var checkTrips = function () {
 					} else if (user !== null) {
 						twilio(user, item);
 						mail(user, item);
+						item.active = false;
+						item.save(function (err) {
+							if (err) {
+								console.log('Error saving change in trip status: ', err);
+							}
+						});
 					}
 					callback();
 				});
