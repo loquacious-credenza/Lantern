802444618d2778b1452ae569360f624c92da2895
Max O'Connell <moconnell7171@gmail.com>
2016-01-27 16:36:03 -0800

    Contact page correctly renders a polyline based off of the trips' path property.

--- ContactPage/app.js
+++ ContactPage/app.js
@@ -2,13 +2,25 @@ var initializeMap = function () {
 	return new google.maps.Map(document.getElementById('map'), {
 		center: {lat:51.508742, lng:-0.120850},
 		zoom:14,
-		minZoom:14,
-		maxZoom:14
+		minZoom:1,
+		maxZoom:100
 	});
 }
 
 var renderLocationsToMap = function (map, data) {
-	console.log("Recieving data: ", data);
+	var points = [];
+	var path;
+	for (var i = 0; i < data.path.length; i++) {
+		points.push(new google.maps.LatLng(data.path[i].location.coordinates[0], data.path[i].location.coordinates[1]));
+	};
+	path = new google.maps.Polyline({
+		path: points,
+		strokeColor: "#0000FF",
+		strokeOpacity: 0.8,
+		strokeWeight: 2
+	});
+
+	path.setMap(map);
 };
 
 var updateLocationData = function (map, targetUrl) {
