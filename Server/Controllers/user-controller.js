187c41bc86f03fe4ae9eaef8e64e8daddf995cfe
Max O'Connell <moconnell7171@gmail.com>
2016-01-28 19:34:09 -0800

    User controller 'findOrCreateUser' modified to include active trips associated with the user.

--- Server/Controllers/user-controller.js
+++ Server/Controllers/user-controller.js
@@ -1,7 +1,8 @@
 var User = require('../Models/user.js');
+var Trip = require('../Models/trip.js');
 
 module.exports = {
-	//FINDS 'user' BY ID AND RETURNS JSON
+	//FINDS 'user' BY ID AND RETURNS JSON.
 	findUser: function (id, res) {
 		User.findById(id, function (err, response) {
 			if (err) {
@@ -12,22 +13,36 @@ module.exports = {
 			}
 		});
 	},
-	// SEARCHES FOR 'user' BY ID INCLUDED IN 'userObj'. IF 'user' IS FOUND, IT IS RETURNED.
-	// IF NOT, 'user' IS CREATED IN THE DATABASE USING 'userObj' AND THEN RETURNED
+	// SEARCHES FOR 'user' BY ID INCLUDED IN 'userObj'. IF 'user' IS FOUND, THE ID ASSOCIATED WITH 'user' IS USED TO SEARCH
+	// FOR AN ACTIVE 'trip'. BOTH 'user', AND 'trip' ARE THEN ATTACHED TO THE 'combinedData' OBJECT, WHICH IS SENT TO THE CLIENT.
+	// IF THE USER IS NOT INITIALLY FOUND, 'user' IS CREATED IN THE DATABASE USING 'userObj' AND THEN RETURNED. IN THIS CASE, 'activeTrip'
+	// DEFAULTS TO 'null'.
 	findOrCreateUser: function (userObj, res) {
+		var combinedData = {};
 		User.findById(userObj._id, function (err, response) {
 			if (err) {
 				console.log("Error finding user: ", err);
 				res.sendStatus(500);
 			} else if (response !== null) {
-				res.json(response);
+				combinedData.user = response;
+				Trip.find({active: true, user_id: response._id}, function (err, response) {
+					if (err) {
+						console.log("Error finding trip associated with user: ", err);
+						res.sendStatus(500);
+					} else {
+						combinedData.activeTrip = response[0] || null;
+						res.json(combinedData);
+					}
+				});
 			} else {
 				User.create(userObj, function (err, response) {
 					if (err) {
 						console.log("Error creating user: ", err);
 						res.sendStatus(500);
 					} else {
-						res.json(response);
+						combinedData.user = response;
+						combinedData.activeTrip = null;
+						res.json(combinedData);
 					}
 				});
 			}
