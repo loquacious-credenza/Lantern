eaee8ddac03aa105a426cf401d0f9a207ba62261
Stephen Saunders <saunders.stephen@gmail.com>
2016-02-04 12:22:42 -0800

    able to check state after Load Active Trip event

--- Client/Src/reducers/active-trip.js
+++ Client/Src/reducers/active-trip.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const {extend} = require('lodash');
 
 const {
@@ -33,57 +32,28 @@ export default (state = initialState, {type, payload}) => {
     case CHECK_IN:
       return null;
     case ADD_WAYPOINT:
-      return extend({}, state, {
-        waypoints: waypointReducer(state.waypoints, {type, payload})
-      });
+      return extend({}, state, {waypoints:state.waypoints.concat([payload])});
+      // return waypointReducer(state, {type, action});
     case LOAD_ACTIVE_TRIP:
-      return extend({}, state, {
+      const nstate = extend({}, state, {
         id: payload.id,
         startTime: payload.startTime,
         eta: payload.eta,
         overdueTime: payload.overdueTime,
         origin: payload.origin,
-        destination: destinationReducer(state, payload.destination),
-        waypoints: waypointReducer(state.waypoints, payload.waypoints)
+        destination: payload.destination,
+        waypoints: state.waypoints.concat(payload.waypoints)
       });
+      console.log('IN THE REDUCER ACTIVE TRIP LOAD ACTIVE TRIP', nstate);
+      return nstate;
     case RESET_DELAY:
       return extend({}, state, {endTime:new Date});
       // TO DO: DOUBLE CHECK THIS LOGIC
     case ADD_START:
-      return extend({}, state, {origin: payload});
+      return extend({}, state, {origin:payload});
     case ADD_DESTINATION:
-      return extend({}, state, {
-        destination: destinationReducer(state.destination, {type, payload})
-      });
+      return extend({}, state, {destination:payload})
     default:
       return state;
   }
 }
-
-const waypointReducer =
-  (state = [], {type, payload}) => {
-  switch(type) {
-    case ADD_WAYPOINT:
-      return state.concat([payload]);
-    case LOAD_ACTIVE_TRIP:
-      return state.concat([payload.waypoints]);
-  };
-};
-
-const destinationReducer =
-  (state = {}, { type, payload }) => {
-  switch(type) {
-    case ADD_DESTINATION:
-    case LOAD_ACTIVE_TRIP:
-      return extend({}, state, {
-        destination: payload
-      });
-  }
-}
-
-
-
-
-
-
-
