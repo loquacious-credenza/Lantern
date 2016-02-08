f9d165169a1db08399d97ad0ed1989b2f9c89dfc
Stephen Saunders <saunders.stephen@gmail.com>
2016-02-08 12:41:30 -0800

    add xcode settings for new webrtc packages

--- Client/android/app/src/main/java/com/lantern/MainActivity.java
+++ Client/android/app/src/main/java/com/lantern/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lantern;
 
 import com.facebook.react.ReactActivity;
+import com.oney.ReactNativeWebRTC.WebRTCModulePackage;
 import com.AirMaps.AirPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -36,6 +37,7 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+        new WebRTCModulePackage(),
         new AirPackage());
     }
 }
