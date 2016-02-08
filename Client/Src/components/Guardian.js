c2beaea6cd7fc89c68432aaa30bf899a164385b7
Stephen Saunders <saunders.stephen@gmail.com>
2016-02-08 12:39:53 -0800

    Incorporating webrtc code into app

--- Client/Src/components/Guardian.js
+++ Client/Src/components/Guardian.js
@@ -5,13 +5,31 @@ import React, {
   Component,
   View,
   Text,
-  TouchableOpacity
+  TouchableOpacity,
+  TouchableHighlight,
+  TextInput
 } from 'react-native';
 import NavBar from './nav-bar';
+
+var WebRTC = require('react-native-webrtc');
+var {
+  RTCPeerConnection,
+  RTCMediaStream,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  RTCSetting,
+} = WebRTC;
+
+window.navigator.userAgent = "react-native";
+const io = require('socket.io-client/socket.io');
+
+const socket = io.connect('http://react-native-webrtc.herokuapp.com');
 
 // importing styles
 import { baseStyles } from '../styles-base';
 const styles = StyleSheet.create(require('../styles.js'));
+const baseStyles = StyleSheet.create(require('../styles.js'));
 const localStyles = StyleSheet.create({
   guardianContainer: {
     position: 'absolute',
@@ -42,30 +60,262 @@ const localStyles = StyleSheet.create({
   }
 });
 
+var configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
+
+var pcPeers = {};
+var localStream;
+
+function getLocalStream(isFront, callback) {
+  console.log('getLocalStream');
+  navigator.getUserMedia({
+    "audio": true,
+    "video": true,
+    "videoType": (isFront ? "front" : "back") // optional, values is `back`, `front`
+  }, function (stream) {
+    callback(stream);
+  }, logError);
+}
+
+function join(roomID) {
+  socket.emit('join', roomID, function(socketIds){
+    console.log('join', socketIds);
+    for (var i in socketIds) {
+      var socketId = socketIds[i];
+      createPC(socketId, true);
+    }
+  });
+}
+
+function createPC(socketId, isOffer) {
+  var pc = new RTCPeerConnection(configuration);
+  pcPeers[socketId] = pc;
+
+  pc.onicecandidate = function (event) {
+    console.log('onicecandidate', event.candidate);
+    if (event.candidate) {
+      socket.emit('exchange', {'to': socketId, 'candidate': event.candidate });
+    }
+  };
+
+  function createOffer() {
+    pc.createOffer(function(desc) {
+      console.log('createOffer', desc);
+      pc.setLocalDescription(desc, function () {
+        console.log('setLocalDescription', pc.localDescription);
+        socket.emit('exchange', {'to': socketId, 'sdp': pc.localDescription });
+      }, logError);
+    }, logError);
+  }
+
+  pc.onnegotiationneeded = function () {
+    console.log('onnegotiationneeded');
+    if (isOffer) {
+      createOffer();
+    }
+  }
+
+  pc.oniceconnectionstatechange = function(event) {
+    console.log('oniceconnectionstatechange', event.target.iceConnectionState);
+  };
+  pc.onsignalingstatechange = function(event) {
+    console.log('onsignalingstatechange', event.target.signalingState);
+  };
+
+  pc.onaddstream = function (event) {
+    console.log('onaddstream', event.stream);
+    container.setState({info: 'One peer join!'});
+    peerConnected();
+
+    var remoteList = container.state.remoteList;
+    remoteList[socketId] = event.stream.toURL();
+    container.setState({ remoteList: remoteList });
+  };
+  pc.addStream(localStream);
+  return pc;
+}
+
+function exchange(data) {
+  var fromId = data.from;
+  var pc;
+  if (fromId in pcPeers) {
+    pc = pcPeers[fromId];
+  } else {
+    pc = createPC(fromId, false);
+  }
+
+  if (data.sdp) {
+    console.log('exchange sdp', data);
+    pc.setRemoteDescription(new RTCSessionDescription(data.sdp), function () {
+      if (pc.remoteDescription.type == "offer")
+        pc.createAnswer(function(desc) {
+          console.log('createAnswer', desc);
+          pc.setLocalDescription(desc, function () {
+            console.log('setLocalDescription', pc.localDescription);
+            socket.emit('exchange', {'to': fromId, 'sdp': pc.localDescription });
+          }, logError);
+        }, logError);
+    }, logError);
+  } else {
+    console.log('exchange candidate', data);
+    pc.addIceCandidate(new RTCIceCandidate(data.candidate));
+  }
+}
+
+function leave(socketId) {
+  console.log('leave', socketId);
+  var pc = pcPeers[socketId];
+  var viewIndex = pc.viewIndex;
+  pc.close();
+  delete pcPeers[socketId];
+
+  var remoteList = container.state.remoteList;
+  delete remoteList[socketId]
+  container.setState({ remoteList: remoteList });
+  container.setState({info: 'One peer leave!'});
+}
+
+
+
+function logError(error) {
+  console.log("logError", error);
+}
+
+function mapHash(hash, func) {
+  var array = [];
+  for (var key in hash) {
+    var obj = hash[key];
+    array.push(func(obj, key));
+  }
+  return array;
+}
+
+function peerConnected() {
+  RTCSetting.setAudioOutput('speaker');
+  RTCSetting.setKeepScreenOn(true);
+  RTCSetting.setProximityScreenOff(true);
+}
+
+let container;
+
 export default class Guardian extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      info: 'Initializing',
+      status: 'init',
+      roomID: '',
+      isFront: true,
+      selfViewSrc: null,
+      remoteList: {},
+      webrtc: false,
+    };
   }
+
+  componentDidMount() {
+    container = this;
+    socket.on('exchange', function(data){
+      exchange(data);
+    });
+    socket.on('leave', function(socketId){
+      leave(socketId);
+    });
+
+    socket.on('connect', function(data) {
+      console.log('connect');
+      getLocalStream(true, function(stream) {
+        localStream = stream;
+        container.setState({selfViewSrc: stream.toURL()});
+        container.setState({status: 'ready', info: 'Please enter or create room ID'});
+      });
+    });
+  }
+
+  _press = (event) => {
+    this.refs.roomID.blur();
+    this.setState({status: 'connect', info: 'Connecting'});
+    join(this.state.roomID);
+  };
+
+  _switchVideoType = () => {
+    var isFront = !this.state.isFront;
+    this.setState({isFront});
+    getLocalStream(isFront, function(stream) {
+      if (localStream) {
+        for (var id in pcPeers) {
+          var pc = pcPeers[id];
+          pc && pc.removeStream(localStream);
+        }
+        localStream.release();
+      }
+      localStream = stream;
+      container.setState({selfViewSrc: stream.toURL()});
+
+      for (var id in pcPeers) {
+        var pc = pcPeers[id];
+        pc && pc.addStream(localStream);
+      }
+    });
+  };
 
   render() {
     const { state, actions, navigator } = this.props;
     const { currentLocation } = state; //destructure the parts of state that you need
     const { getCurrentLocation } = actions; // destructure the actions the components uses to update state.
+    const webrtc = this.state.webrtc ?
+      (<View style={styles.container}>
+        <Text style={styles.welcome}>
+          {this.state.info}
+        </Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text>
+            {this.state.isFront ? "Use front camera" : "Use back camera"}
+          </Text>
+          <TouchableHighlight
+            style={{borderWidth: 1, borderColor: 'black'}}
+            onPress={this._switchVideoType}>
+            <Text>Switch camera</Text>
+          </TouchableHighlight>
+        </View>
+        { this.state.status == 'ready' ?
+          (<View>
+            <TextInput
+              ref='roomID'
+              autoCorrect={false}
+              style={{width: 200, height: 40, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text) => this.setState({roomID: text})}
+              value={this.state.roomID}
+            />
+            <TouchableHighlight
+              onPress={this._press}>
+              <Text>Enter room</Text>
+            </TouchableHighlight>
+          </View>) : null
+        }
+        <RTCView streamURL={this.state.selfViewSrc} style={styles.selfView}/>
+        {
+          mapHash(this.state.remoteList, function(remote, index) {
+            return <RTCView key={index} streamURL={remote} style={styles.remoteView}/>
+          })
+        }
+      </View>) : null;
+    const streamToggle = !this.state.webrtc ?
+      (<View style={localStyles.guardianContainer}>
+          <NavBar
+            navigator={navigator}
+            description='Guardian'
+          />
+          <TouchableOpacity onPress={() => {
+            this.setState({webrtc: true});
+          }}>
+            <View style={localStyles.circle}>
+            </View>
+          </TouchableOpacity>
+        </View>) : null;
 
     return (
       <View style={baseStyles.navContainer}>
-      <View style={localStyles.guardianContainer}>
-        <NavBar
-          navigator={navigator}
-          description='Guardian'
-        />
-        <TouchableOpacity onPress={() => {
-          navigator.pop()
-        }}>
-          <View style={localStyles.circle}>
-          </View>
-        </TouchableOpacity>
-      </View>
+      {webrtc}
+      {streamToggle}
       </View>
     );
   }
