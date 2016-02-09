530cd24d01966f371828fdccf929d0ccb827d494
Rebecca Yang <rebeccayang123@gmail.com>
2016-02-08 23:14:18 -0800

    Tutorial views complete

---
+++ Client/Src/constants/tutorial-content.js
@@ -0,0 +1,29 @@
+module.exports = {
+  0: {
+    title: 'Welcome to Lantern!',
+    body: "Our goal is to help ensure you get home safely." 
+          + " If you don't make it home within a reasonable amount of time, we will notify your loved ones."
+          + " Before we can get started, we need to go over some housekeeping items."
+  },
+  1: {
+    title: 'Lantern is not a substitute for 911',
+    body: "If you feel unsafe, you need to call 911, which is the fastest and most reliable way for you to get help."
+          + " Under no circumstances should Lantern be considered a substitute for dialing 911." 
+  },
+
+  2: {
+    title: 'How Lantern works',
+    body: "Before you depart, tell us where you're going and how long you think it will take. "
+    +"We will keep track of and store your location as you make your way to your destination."
+  },
+  3: {
+    title: 'Acceptable delay',
+    body: "We use acceptable delay to calibrate how much later you can be to your destination" 
+          + " than what you anticipated in your ETA."
+  },
+  4: {
+    title: 'Emergency contacts',
+    body: "Give us the name and number of up to five emergency contacts. If you do not make it to your destination before your ETA and Acceptable Delay" 
+          + " have passed, we will notify your emergency contacts of this."
+  }
+}
